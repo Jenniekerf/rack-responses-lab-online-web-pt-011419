17f3d5d99a57b61cb4389ee1fc3b652dc3c2d5b6
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-04-08 17:48:43 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,8 +3,8 @@ class Application
   def call(env)
     resp = Rack::Response.new
     
-    resp.write 
-    
+    resp.write "Good Morning!"
+    resp.write "Good Afternoon!"
     
     resp.finish
   end
